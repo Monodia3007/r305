a2c7eb85cb33a0ffaf2b7a47d7c91a921ef455a9
Pierre Camplin <pierre.camplin.etu@univ-lille.fr>
2023-11-14 09:41:54 +0100

    Add detailed descriptions to the tp3.h header file
    This commit adds detailed descriptions of the prototype functions used in the tp3.h header file. Documenting these details provides clear information about the functions including their purpose, parameters, and expected returns. This change aids in the readability and maintainability of our code base.

--- tp3/tp3.h
+++ tp3/tp3.h
@@ -1,9 +1,44 @@
+/**
+ * @file tp3.h
+ * @brief Header file for TP3 in Project r305.
+ *
+ * This header file contains the prototypes for the functions used in the tp3.c source file.
+ */
+
 #ifndef R305_TP3_H
 #define R305_TP3_H
 
+/**
+ * @brief Prototype for function to display file or directory information.
+ *
+ * @param chemin The path of the file or directory.
+ * @return Returns 0 if successful; otherwise, it returns -1.
+ */
 int afficher_infos(const char *chemin);
+
+/**
+ * @brief Prototype for function to display the contents of a directory.
+ *
+ * @param chemin The path of the directory.
+ * @return Returns 0 if successful; otherwise, it returns -1.
+ */
 int afficher_repertoire(const char *chemin);
+
+/**
+ * @brief Prototype for function to spawn a child process to execute the `afficher_repertoire` function.
+ *
+ * @param chemin The path of the directory to list.
+ * @return Returns the PID of the child process if successful; otherwise, it returns -1.
+ */
 int lancer_traitement(const char *chemin);
+
+/**
+ * @brief Prototype for the main function.
+ *
+ * @param argc The number of arguments.
+ * @param argv The argument vector.
+ * @return Returns 0 if successful; otherwise, it returns 1.
+ */
 int main(int argc, char *argv[]);
 
 #endif //R305_TP3_H
