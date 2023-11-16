0934e3f9e916345ede5f0bb09ab20ef33d0a18ac
Lilith Camplin <lilith.camplin@gmail.com>
2023-11-16 19:51:33 +0100

    Implement thread-safe memory allocation test
    Enhanced the `run_infinite_malloc` function by threading it with 'scan_key', which listens for a 'q' command to stop the continuous allocation. This enables us to have control over the memory allocation, preventing possible crashes or hangs due to exhaustive memory use. Modified CMakeLists for the inclusion of the necessary Thread library.

--- src/test/no_ram_for_you.c
+++ src/test/no_ram_for_you.c
@@ -1,17 +1,41 @@
-//
-// Created by Lilith on 16/11/2023.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "no_ram_for_you.h"
+
+volatile int cancel = 0;
+
+/**
+ * @brief Function to continuously allocate memory using malloc().
+ *
+ * This file contains the implementation of a function that continuously
+ * allocates memory using malloc(). The function runs indefinitely until an
+ * error occurs during the memory allocation.
+ */
+void* scan_key(void* arg) {
+    char key;
+    while(1) {
+        key = getchar();
+        if(key == 'q') {
+            cancel = 1;
+            return NULL;
+        }
+    }
+}
 
 void run_infinite_malloc(void) {
     int ram_consumed = 0;
-    while (1)
+    pthread_t tid;
+
+    // Create a separate thread to listen for the 'q' key
+    pthread_create(&tid, NULL, &scan_key, NULL);
+
+    while (!cancel)
     {
         ram_consumed += 1;
         malloc(1024);
         printf("%dMio\n", ram_consumed);
     }
+
+    pthread_join(tid, NULL);  // make sure the thread ends before exiting the function
 }
