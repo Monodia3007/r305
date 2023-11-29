3c2b8512ce42221018d0785d9fb93b34b8626a39
Lilith Camplin <lilith.camplin@gmail.com>
2023-11-29 19:39:18 +0100

    Add user confirmation before running potentially dangerous functions
    A new function "confirm_execution" has been added that prompts the user for confirmation before running any potentially harmful function. This prompt will appear before running "run_infinite_fork" and "run_infinite_malloc" functions in `no_ram_for_you.c`, providing users more control and safety.

--- src/test/no_ram_for_you.c
+++ src/test/no_ram_for_you.c
@@ -2,10 +2,33 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 #include "no_ram_for_you.h"
 
 volatile int cancel = 0;
 
+/**
+ * @brief Function to confirm the execution of a potentially dangerous operation.
+ *
+ * This function prompts the user to type 'y' or 'yes' before proceeding. If the user does not confirm,
+ * the function will return 0, otherwise it will return 1.
+ *
+ * @return int Returns 1 if the user confirms the execution, 0 otherwise.
+ */
+int confirm_execution(void)
+{
+    char input[4];
+
+    printf("Are you sure you want to proceed? (y/yes to confirm): ");
+    fgets(input, 4, stdin);
+
+    if (strcmp(input, "y\n") == 0 || strcmp(input, "yes\n") == 0)
+    {
+        return 1;
+    }
+
+    return 0;
+}
 
 /**
  * @brief Scans the keyboard input in a separate thread
@@ -38,6 +61,12 @@ void *scan_key()
  */
 void run_infinite_malloc(void)
 {
+    if (!confirm_execution())
+    {
+        printf("Execution cancelled.\n");
+        return;
+    }
+
     int ram_consumed = 0;
     pthread_t tid;
 
@@ -55,8 +84,24 @@ void run_infinite_malloc(void)
     pthread_join(tid, NULL);  // make sure the thread ends before exiting the function
 }
 
+/**
+ * @brief Function to continuously allocate memory and create new processes using fork().
+ *
+ * This function continuously allocates memory using malloc() and creates new processes using fork().
+ * The function runs indefinitely in a while loop.
+ *
+ * The function does not return anything and is marked with the _Noreturn keyword.
+ *
+ * @note This function can quickly consume system resources and should be used with caution.
+ */
 _Noreturn void run_infinite_fork(void)
 {
+    if (!confirm_execution())
+    {
+        printf("Execution cancelled.\n");
+        exit(0);
+    }
+
     int ram_consumed = 0;
     while (1)
     {
